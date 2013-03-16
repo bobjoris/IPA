110122046532bed63c249c19a5ea432d56818224
ClemC <ent.melc@gmail.com>
2013-03-16 15:22:46 +0100

    Update binarytree_sdl.c
    Algorithme ajouté: animation d'un noeud d'un point A à un point B de façon rectiligne

--- binarytree_sdl.c
+++ binarytree_sdl.c
@@ -141,3 +141,59 @@ void drawCircle(SDL_Surface *surface, int n_cx, int n_cy, int radius, Uint32 pix
     }
 }
 
+void AnimOneNode(SDL_Surface *surface, int radius, Uint32 pixel, int node, int x1, int y1, int x2, int y2){
+	int x=x1;
+	int y=y1;
+	float p=(y2-y1)/(x2-x1);//pente entre les deux points
+	int qx=0, qy=0;//pente entière
+	if(p<1){
+		//equivalent à mettre p sous la forme d'une fraction 1 sur a, on aura qx=a
+		switch((int)(log(p))){
+			case -1:
+			qx=(int)(10*p);
+			break;
+			
+			case -2:
+			qx=(int)(100*p);
+			break;
+			
+			case -3:
+			qx=(int)(1000*p);
+			break;
+			
+			default;
+			qx=0;
+			break;
+		}
+	}
+	else{
+		qy=(int)p;
+	}
+	while((x!=x2) &&(y!=y2)){
+		drawCircle(surface,x,y,radius,pixel);
+		if(qx=0){//pente >1
+			y=y+qy;
+			x++;
+		}
+		if(qy=0){//pente <1
+			x=x+qx;
+			y++;
+		}
+		if(abs(x-x2)<2 && abs(y-y2)<2){//troisième cas ou on est tres proche du point final
+			drawCircle(surface,x2,y2,radius,pixel);
+			x=x2;
+			y=y2;
+		}
+	}
+}
+
+void printAnim2nodes (int node, BinaryTree tree1, BinaryTree tree2){
+	//Animation déplacement rectiligne du noeud présent dans les deux arbres
+	int x1=getPosition(node,tree1);//fonction getPosition à faire
+	int y1=getPosition(node,tree1);
+	int x2=getPosition(node,tree2);
+	int y2=getPosition(node,tree2);
+	AnimOneNode(surface, radius, pixel, node, x1, y1, x2, y2);
+}
+
+
