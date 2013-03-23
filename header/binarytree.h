0cc72cc10998685db14f41b254ba8f555526069c
ClemC <ent.melc@gmail.com>
2013-03-23 18:33:55 +0100

    Update binarytree.h
    mise a jour des prototypes concernant la suppression de noeuds

--- header/binarytree.h
+++ header/binarytree.h
@@ -28,11 +28,25 @@ BinaryTree* createNode(int val);
 BinaryTree* createNodeWithChilds(BinaryTree *left, int val, BinaryTree *right);
 // Insère un noeud dans l'arbre passé en paramètre
 BinaryTree* insertNode(int val, BinaryTree *tree);
-//Insère un noeud avec une rotation finale
+// Insère un noeud avec une rotation finale
 BinaryTree* insertNodeWithRotation(int val, BinaryTree *tree);
+// Compte le nombre de fils d'un noeud dans un arbre
+int numberOfLeaf(BinaryTree* tree, int val);
+// Renvoie la valeur maximale des feuilles d'un arbre
+int maxNode(BinaryTree* tree);
+// Remplace la valeur d'un noeud par une autre
+BinaryTree* setNode(int val, int intoval, BinaryTree* tree);
+// Echange les valeurs de 2 noeuds
+BinaryTree* switchNode(int val1, int val2, BinaryTree* tree);
+// Supprime un noeud d'arité 1
+BinaryTree* deleteNode1(int val, BinaryTree *tree);
+// Supprime un noeud d'arité 2
+BinaryTree* deleteNode2(int val, BinaryTree *tree);
 // Supprime une feuille dans un arbre
 BinaryTree* deleteLeaf(int val, BinaryTree *tree);
-//Copie d'un arbre
+// Supprime un noeud dans un arbre
+BinaryTree* deleteNode(int val, BinaryTree *tree);
+// Copie d'un arbre
 BinaryTree* copyTree(BinaryTree * tree);
 
 /*
